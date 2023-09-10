f2d3ba5f73867984469660394469b57958d576f0
bakunts8 <khachikbakunts@gmail.com>
2023-09-10 22:19:39 +0400

    Add Pojo class

--- src/main/java/ApiPages/Pojo.java
+++ src/main/java/ApiPages/Pojo.java
@@ -1,19 +1,18 @@
 package ApiPages;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Pojo {
 
-    private int id;
+
     private String email;
-    private String first_name;
-    private String last_name;
+    private String name;
+    private String gender;
+    private String status;
 
-    public int getId() {
-        return id;
-    }
+    ObjectMapper objectMapper = new ObjectMapper();
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getEmail() {
         return email;
@@ -23,19 +22,55 @@ public class Pojo {
         this.email = email;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getName() {
+        return name;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getGender() {
+        return gender;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String serializationPojo(String email, String name, String gender, String status) throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        setEmail(email);
+        setGender(gender);
+        setName(name);
+        setStatus(status);
+        return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(this);
+    }
+
+    public String serializationPojo(String email, String name) throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        setEmail(email);
+        setName(name);
+        return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(this);
+    }
+
+    public void deserialization(String email, String name, String gender, String status) throws JsonProcessingException {
+
+        System.out.println("DeSerialization...");
+        Pojo pojo = objectMapper.readValue(serializationPojo("lenox_lewis@wbc.com", "Lenox Lewis", "male", "active"), Pojo.class);
+        System.out.println("name : " + pojo.getName());
+        System.out.println("email : " + pojo.getEmail());
+        System.out.println("gender : " + pojo.getGender());
+        System.out.println("status : " + pojo.getStatus());
     }
 }
