82ea39691df64a156963cc0414febd614d3a0f4d
bakunts8 <khachikbakunts@gmail.com>
2023-10-17 19:08:59 +0400

    Add new solution

--- src/main/java/org/example/LongSequence.java
+++ src/main/java/org/example/LongSequence.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class Testing {
+import java.util.*;
+
+public class LongSequence {
+
+    public static void main(String[] args) {
+        int[] array = {7, 7, 8, 2, 1, 5, 1, 1, 3, 4, 2, 2, 81, 82, 83, 100, 1, 3, 4, 4, 1, 4, 9, 10, 11, 12, 13, 14, 15};
+        int[] nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        int[] numbers = {9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6};
+        System.out.println(longest(array));
+        System.out.println(longest(nums));
+        System.out.println(longest(numbers));
+    }
+
+    static int longest(int[] array) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        for (Integer i : array) {
+            numbers.add(i);
+        }
+        int length = 0;
+        int x = 1;
+
+        for (Integer i : numbers) {
+            if (numbers.contains(i + 1)) {
+                x++;
+            } else {
+                x = 1;
+            }
+            length = x > length ? x : length;
+        }
+        System.out.println(numbers);
+        return length;
+    }
 }
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,9 +4,6 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        int[] array = {7, 7, 8, 2, 1, 5, 1, 1, 3, 4, 2, 2, 81, 82, 83, 100, 1, 3, 4, 4, 1, 4, 9, 10, 11, 12, 13, 14, 15};
-
-        longest(array);
     }
 
     static void print(int[] array) {
@@ -31,41 +28,6 @@ public class Test {
         for (int i = 0; i < 7; i++) {
             s = String.valueOf(a.append("*"));
             System.out.println(s);
-        }
-    }
-
-    static void longest(int[] array) {
-        TreeSet<Integer> numbers = new TreeSet<>();
-        for (Integer i : array) {
-            numbers.add(i);
-        }
-
-        int[] arr = new int[numbers.size()];
-        int index = -1;
-        for (Integer i : numbers) {
-            index++;
-            arr[index] = i;
-        }
-
-        List<Integer> sequenceEnds = new LinkedList<>();
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] > 1) {
-                sequenceEnds.add(i);
-            }
-        }
-
-        int x = 0;
-        int y = sequenceEnds.get(0);
-        for (int i = 1; i < sequenceEnds.size(); i++) {
-            if (sequenceEnds.get(i) - sequenceEnds.get(i - 1) > y - x) {
-                x = sequenceEnds.get(i - 1);
-                y = sequenceEnds.get(i);
-            }
-        }
-
-        System.out.println(numbers);
-        for (int i = x; i < y; i++) {
-            System.out.print(arr[i] + " ");
         }
     }
 }
